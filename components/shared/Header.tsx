e0d7c4b73bd741d2b2da32f1a731b8a6faef1788
omid <itmanager.abbasi@gmail.com>
2024-06-24 15:13:01 +0330

    Add Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
     return (
@@ -11,9 +13,16 @@ const Header = () => {
                     <Image src="/assets/images/logo.svg" alt="Evently logo" width={128} height={38} />
                 </Link>
 
+                <SignedIn>
+                    <nav className="md:flex-between hidden w-full max-w-xs">
+                        <NavItems/>
+                    </nav>
+                </SignedIn>
+
                 <div className="flex justify-end gap-3 w-32">
                     <SignedIn>
                         <UserButton afterSignOutUrl="/"/>
+                        <MobileNav/>
                     </SignedIn>
                     <SignedOut>
                         <Button asChild className="rounded-full" size="lg">
