800090dccf98d5981d914bad9b429673897e7427
omid abbasi <itmanager.abbasi@gmail.com>
2024-06-25 18:58:09 +0330

    Finish NavItems And Footer

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const NavItems = () => {
+  const pathname = usePathname();
+
   return (
-    <div>NavItems</div>
+    <ul className='md:flex-between flex flex-col w-full md:flex-row gap-5 items-start'>
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return <li key={link.route} className={`${isActive && 'text-primary-500'}
+           flex-center p-medium-16 whitespace-nowrap`}><Link href={link.route}>{link.label}</Link></li>
+      })}
+    </ul>
   )
 }
 
