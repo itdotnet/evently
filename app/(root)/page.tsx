267a5ac3de6fac340ba1268b99ac429c67203248
omid <itmanager.abbasi@gmail.com>
2024-07-07 14:19:20 +0330

    add collection

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const events=await getAllEvents({
+    query:'',
+    category:'',
+    page:1,
+    limit:6
+  });
+
+
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -29,7 +39,7 @@ export default function Home() {
           CategoryFilter
         </div>
 
-        <Collection data={[]} emptyTitle="No Events Found" emptyStateSubtext="Come back later"
+        <Collection data={events?.data} emptyTitle="No Events Found" emptyStateSubtext="Come back later"
           collectionType="All_Events" limit={6} page={1} totalPages={2} />
       </section>
     </>
