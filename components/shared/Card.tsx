3d0c01014c5628ba10e574ee12abfa4d33b5e78f
omid <itmanager.abbasi@gmail.com>
2024-07-08 14:26:49 +0330

    Add profile page

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -13,10 +13,10 @@ type CardProps = {
 }
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
-    const {sessionClaims}=auth();
-    const userId=sessionClaims?.userId as string;
+    const { sessionClaims } = auth();
+    const userId = sessionClaims?.userId as string;
 
-    const isEventCreator=userId===event.organizer._id.toString();
+    const isEventCreator = userId === event.organizer._id.toString();
 
     return (
         <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]'>
@@ -29,14 +29,14 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             {isEventCreator && !hidePrice && (
                 <div className='absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all'>
                     <Link href={`/events/${event._id}/update`}>
-                        <Image src='/assets/icons/edit.svg' alt='edit' width={20} height={20}/>
+                        <Image src='/assets/icons/edit.svg' alt='edit' width={20} height={20} />
                     </Link>
 
-                    <DeleteConfirmation eventId={event._id}/>
+                    <DeleteConfirmation eventId={event._id} />
                 </div>
             )}
 
-            <Link href={`/events/${event._id}`} className='flex min-h-230 flex-col gap-3 p-5 md:gap-4'>
+            <div className='flex min-h-230 flex-col gap-3 p-5 md:gap-4'>
                 {!hidePrice &&
                     <div className='flex gap-2'>
                         <span className='p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60'>
@@ -52,7 +52,9 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                     {formatDateTime(event.startDateTime).dateTime}
                 </p>
 
-                <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+                <Link href={`/events/${event._id}`}>
+                    <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+                </Link>
 
                 <div className='flex-between w-full'>
                     <p className='p-medium-14 md:p-medium-16 text-grey-600'>
@@ -62,11 +64,11 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                     {hasOrderLink && (
                         <Link href={`/orders?eventId=${event._id}`} className='flex gap-2'>
                             <p className='text-primary-500'>Order Details</p>
-                            <Image src='/assets/icons/arrow.svg' alt='search' width={10} height={10}/>
+                            <Image src='/assets/icons/arrow.svg' alt='search' width={10} height={10} />
                         </Link>
                     )}
                 </div>
-            </Link>
+            </div>
         </div>
     )
 }
