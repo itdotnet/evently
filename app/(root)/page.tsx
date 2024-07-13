752c46a78dcfecedc1a66c470927e743fa145571
omid <itmanager.abbasi@gmail.com>
2024-07-13 16:12:29 +0330

    Add orders page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
@@ -35,7 +36,7 @@ export default async function Home() {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex flex-col w-full gap-5 md:flex-row">
-          Search
+          <Search/>
           CategoryFilter
         </div>
 
