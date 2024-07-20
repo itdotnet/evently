72be1979010ed941a4e0aab024705b7365458e38
omid abbasi <itmanager.abbasi@gmail.com>
2024-07-20 16:11:37 +0330

    fixed order.actions.ts

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,14 +1,14 @@
 "use server"
 
-import Stripe from "stripe";
+import Stripe from 'stripe';
 import { CheckoutOrderParams, CreateOrderParams, GetOrdersByEventParams, GetOrdersByUserParams } from "@/types"
-import { redirect } from "next/navigation";
-import { handleError } from "../utils";
-import { connectToDatabase } from "../database";
-import Order from "../database/models/order.model";
-import Event from "../database/models/event.model";
+import { redirect } from 'next/navigation';
+import { handleError } from '../utils';
+import { connectToDatabase } from '../database';
+import Order from '../database/models/order.model';
+import Event from '../database/models/event.model';
 import { ObjectId } from 'mongodb';
-import User from "../database/models/user.model";
+import User from '../database/models/user.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -51,7 +51,7 @@ export const createOrder = async (order: CreateOrderParams) => {
         const newOrder = await Order.create({
             ...order,
             event: order.eventId,
-            buyer: order.buyerId
+            buyer: order.buyerId,
         });
 
         return JSON.parse(JSON.stringify(newOrder));
@@ -99,7 +99,7 @@ export async function getOrdersByEvent({ searchString, eventId }: GetOrdersByEve
                     eventTitle: '$event.title',
                     eventId: '$event._id',
                     buyer: {
-                        $concat: ['$buyer.firstName', ' ', '$buyer.lastName'],
+                        $concat: ['$buyer.firstName', { $ifNull: ['$buyer.lastName', ''] }],
                     },
                 },
             },
